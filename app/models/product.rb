9a1829ed264962fa4b17d004d3a68cf49ab7a9c0
“dkx1982” <dkx1982@163.com>
2017-01-19 22:38:56 +0800

    implement image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,5 @@ class Product < ApplicationRecord
   validates :quantity,presence:true
   validates :price, presence: true
   validates :price, numericality: { greater_than: 0 }
+  mount_uploader :image, ImageUploader
 end
